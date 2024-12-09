92b5d2137fefc20c1285413fe1f947ce22844ed2
Virginia <ning93315@gmail.com>
2024-12-09 19:59:25 +0000

    edit popular items

--- assignment/app/home/page.tsx
+++ assignment/app/home/page.tsx
@@ -3,14 +3,9 @@ import Header from "../components/Header";
 import "../home.css";
 import { TiStar } from "react-icons/ti";
 import Footer from "../components/footer";
-import {
-   Carousel,
-   CarouselContent,
-   CarouselItem,
-   CarouselNext,
-   CarouselPrevious,
-} from "@/components/ui/carousel"
-import { Card, CardContent } from "@/components/ui/card"
+
+
+
 
 export default function Home() {
    return <>
@@ -35,99 +30,94 @@ export default function Home() {
          </div>
       </div>
 
-      <div className="flex flex-col w-full justify-center items-center">
+      <div className="flex flex-col w-full justify-center items-center crystal-container  ">
          <h1>Popular</h1>
          <div id="carouselExample" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
                <div className="carousel-item active">
-                  <div className="col-12 col-md-6 col-lg-4">
-                     <div className="card">
-                        <img src="image/Himalaya.png" className="card-img-top" alt="Himalaya Citrine" />
-                        <div className="card-body text-center">
-                           <div className="card-text">Himalaya Citrine</div>
+                  <div className="row">
+                     {/* 每一行顯示四個項目 */}
+                     <div className="col-12 col-md-6 col-lg-3">
+                        <div className="crystalCard">
+                           <img src="image/Himalaya.png" className="card-img-top" alt="Himalaya Citrine" />
+                           <div className="card-body text-center">
+                              <div className="cardC-text">Himalaya Citrine</div>
+                           </div>
+                        </div>
+                     </div>
+
+                     <div className="col-12 col-md-6 col-lg-3">
+                        <div className="crystalCard">
+                           <img src="image/Sphere.png" className="card-img-top" alt="Purple Mica Sphere" />
+                           <div className="card-body text-center">
+                              <div className="cardC-text">Purple Mica Sphere</div>
+                           </div>
+                        </div>
+                     </div>
+
+                     <div className="col-12 col-md-6 col-lg-3">
+                        <div className="crystalCard">
+                           <img src="image/RainbowFluorite.png" className="card-img-top" alt="Rainbow Fluorite" />
+                           <div className="card-body text-center">
+                              <div className="cardC-text">Rainbow Fluorite</div>
+                           </div>
+                        </div>
+                     </div>
+
+                     <div className="col-12 col-md-6 col-lg-3">
+                        <div className="crystalCard">
+                           <img src="image/ClearQuartz.png" className="card-img-top" alt="Clear Quartz" />
+                           <div className="card-body text-center">
+                              <div className="cardC-text">Clear Quartz</div>
+                           </div>
                         </div>
                      </div>
                   </div>
                </div>
 
+               {/* 第二頁 */}
                <div className="carousel-item">
-                  <div className="col-12 col-md-6 col-lg-4">
-                     <div className="card">
-                        <img src="image/Sphere.png" className="card-img-top" alt="Purple Mica Sphere" />
-                        <div className="card-body text-center">
-                           <div className="card-text">Purple Mica Sphere</div>
+                  <div className="row">
+                     <div className="col-12 col-md-6 col-lg-3">
+                        <div className="crystalCard">
+                           <img src="image/Xuebaoding.png" className="card-img-top" alt="Xuebaoding Aquamarine" />
+                           <div className="card-body text-center">
+                              <div className="cardC-text">Xuebaoding Aquamarine</div>
+                           </div>
                         </div>
                      </div>
-                  </div>
-               </div>
 
-               <div className="carousel-item">
-                  <div className="col-12 col-md-6 col-lg-4">
-                     <div className="card">
-                        <img src="image/RainbowFluorite.png" className="card-img-top" alt="Rainbow Fluorite" />
-                        <div className="card-body text-center">
-                           <div className="card-text">Rainbow Fluorite</div>
+                     <div className="col-12 col-md-6 col-lg-3">
+                        <div className="crystalCard">
+                           <img src="image/BlueFluorite.png" className="card-img-top" alt="Blue Fluorite" />
+                           <div className="card-body text-center">
+                              <div className="cardC-text">Blue Fluorite</div>
+                           </div>
                         </div>
                      </div>
-                  </div>
-               </div>
 
-               <div className="carousel-item">
-                  <div className="col-12 col-md-6 col-lg-4">
-                     <div className="card">
-                        <img src="image/ClearQuartz.png" className="card-img-top" alt="Clear Quartz" />
-                        <div className="card-body text-center">
-                           <div className="card-text">Clear Quartz</div>
+                     <div className="col-12 col-md-6 col-lg-3">
+                        <div className="crystalCard">
+                           <img src="image/Azurite.png" className="card-img-top" alt="Azurite" />
+                           <div className="card-body text-center">
+                              <div className="cardC-text">Azurite</div>
+                           </div>
                         </div>
                      </div>
-                  </div>
-               </div>
 
-               <div className="carousel-item">
-                  <div className="col-12 col-md-6 col-lg-4">
-                     <div className="card">
-                        <img src="image/Xuebaoding.png" className="card-img-top" alt="Xuebaoding Aquamarine" />
-                        <div className="card-body text-center">
-                           <div className="card-text">Xuebaoding Aquamarine</div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-
-               <div className="carousel-item">
-                  <div className="col-12 col-md-6 col-lg-4">
-                     <div className="card">
-                        <img src="image/BlueFluorite.png" className="card-img-top" alt="Blue Fluorite" />
-                        <div className="card-body text-center">
-                           <div className="card-text">Blue Fluorite</div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-
-               <div className="carousel-item">
-                  <div className="col-12 col-md-6 col-lg-4">
-                     <div className="card">
-                        <img src="image/Azurite.png" className="card-img-top" alt="Azurite" />
-                        <div className="card-body text-center">
-                           <div className="card-text">Azurite</div>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-
-               <div className="carousel-item">
-                  <div className="col-12 col-md-6 col-lg-4">
-                     <div className="card">
-                        <img src="image/Indian.png" className="card-img-top" alt="Indian Epi Stilbite" />
-                        <div className="card-body text-center">
-                           <div className="card-text">Indian Epi Stilbite</div>
+                     <div className="col-12 col-md-6 col-lg-3">
+                        <div className="crystalCard">
+                           <img src="image/Indian.png" className="card-img-top" alt="Indian Epi Stilbite" />
+                           <div className="card-body text-center">
+                              <div className="cardC-text">Indian Epi Stilbite</div>
+                           </div>
                         </div>
                      </div>
                   </div>
                </div>
             </div>
 
+            {/* 控制按鈕 */}
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                <span className="visually-hidden">Previous</span>
